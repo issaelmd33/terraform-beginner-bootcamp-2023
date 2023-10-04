b13623fd4dd7f2b428992bcbe343dd94d97fa69f
issaelmd33 morales <issael18@gmail.com>
2023-10-04 00:27:49 +0000

    #9 create a s3 bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,36 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
 
+provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
+  # Configuration options
+}
+
+resource "random_string" "bucket_name" {
+  lower = true 
+  upper = false 
+  length           = 32
+  special          = false
+  
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result 
+}
+
+output "random_bucket_name" {
+  value = random_string.bucket_name.result
+}
